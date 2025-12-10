08db2a1d7fa5895ac8f356552bf72d8019c36867
Matteo Sandrin <sandrin98@gmail.com>
2025-12-10 18:47:48 -0500

    Remove old image serving route

--- backend/app/routers/__init__.py
+++ backend/app/routers/__init__.py
@@ -1,5 +1,5 @@
 from .canvas import router as canvas_router
-from .images import router as images_router, ImageCacheMiddleware
+from .images import router as images_router
 from .settings import router as settings_router
 from .jobs import router as jobs_router
 from .analytics import router as analytics_router
@@ -9,7 +9,6 @@ from .update import router as update_router
 __all__ = [
     "canvas_router",
     "images_router",
-    "ImageCacheMiddleware",
     "settings_router",
     "jobs_router",
     "analytics_router",

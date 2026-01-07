f4f528ee7fb111d6e2307022b662b59632cab713
Matteo Sandrin <sandrin98@gmail.com>
2026-01-07 10:40:47 -0500

    Use staticfiles for serving images

--- backend/app/routers/__init__.py
+++ backend/app/routers/__init__.py
@@ -4,6 +4,7 @@ from .settings import router as settings_router
 from .jobs import router as jobs_router
 from .analytics import router as analytics_router
 from .notify import router as notify_router
+from .update import router as update_router
 
 __all__ = [
     "canvas_router",
@@ -12,4 +13,5 @@ __all__ = [
     "jobs_router",
     "analytics_router",
     "notify_router",
+    "update_router",
 ]
